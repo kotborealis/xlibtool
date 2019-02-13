616cf99fac19cc11e345ee87d4e570d3edcf2865
kotborealis <kotborealis@awooo.ru>
2019-02-13 17:30:49 +0300

    refactoring

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 main:
-	gcc -std=c99 main.c -o xlibtool -L/usr/X11/lib -lX11
+	gcc -std=c99 main.c xlibtool.c -o xlibtool -L/usr/X11/lib -lX11
---
+++ xlibtool.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <X11/Xlib.h>
+
+int is_window_hidden(Display *display, Window window);
+int match_window_name(Display *display, Window window, const char *needle);
+Window window_from_name_search(Display *display, Window current, char const *needle);
+Window window_from_name(Display *display, char const *name);
